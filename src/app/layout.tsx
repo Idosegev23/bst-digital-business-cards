0ce61c21a62c7b11199ddf3ac8b6dc0d3b46d977
Idosegev23 <ido.segev23@gmail.com>
2025-08-05 17:31:10 +0300

    jjj

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,23 @@ import { LanguageProvider } from '@/contexts/LanguageContext'
 export const metadata: Metadata = {
   title: 'קבוצת BST - כרטיסי ביקור דיגיטליים',
   description: 'כרטיסי ביקור דיגיטליים של צוות קבוצת BST',
+  icons: {
+    icon: '/BST-LOGO-NEW.svg',
+    shortcut: '/BST-LOGO-NEW.svg',
+    apple: '/BST-LOGO-NEW.svg',
+  },
+  openGraph: {
+    title: 'קבוצת BST - כרטיסי ביקור דיגיטליים',
+    description: 'כרטיסי ביקור דיגיטליים של צוות קבוצת BST',
+    images: '/BST-LOGO-NEW.svg',
+    siteName: 'BST Group',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'קבוצת BST - כרטיסי ביקור דיגיטליים',
+    description: 'כרטיסי ביקור דיגיטליים של צוות קבוצת BST',
+    images: '/BST-LOGO-NEW.svg',
+  },
 }
 
 export const viewport: Viewport = {
