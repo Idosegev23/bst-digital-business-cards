19abbae4fe9e6f3f3d6a3db4d3a87fe485d195f4
Idosegev23 <ido.segev23@gmail.com>
2025-08-17 14:28:02 +0300

    add 2 more

--- src/data/employees.ts
+++ src/data/employees.ts
@@ -401,7 +401,7 @@ export const employees: Employee[] = [
   {
     id: 'linoy-benyamini',
     name: 'לינוי בנימיני',
-    title: 'עוזרת אישית למנכ"ל - קבוצת BST, מנכ"ל תפעול - קרן עווה ובאדי טנוס (ע"ר)',
+    title: 'עוזרת אישית למנכ"ל - קבוצת BST, מנכ"ל תפעול - קרן אווה ובדיע טנוס (ז"ל)',
     photo: '/employees/placeholder.svg',
     email: 'linoyb@bst.co.il',
     phone: '054-8161941',
